c995fbf6098548d1556ab2180f215f78d7327821
Youn Mélois <youn@melois.dev>
2026-04-16 19:01:06 +0200

    patch: pacific-revert-rgw-missing-etag-multipart-upload-result.patch
    Signed-off-by: Youn Mélois <youn@melois.dev>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -6711,9 +6711,6 @@ void RGWCompleteMultipart::complete()
       ldpp_dout(this, 0) << "WARNING: failed to unlock " << serializer->oid << dendl;
     }
   }
-
-  etag = s->object->get_attrs()[RGW_ATTR_ETAG].to_str();
-
   send_response();
 }
 
